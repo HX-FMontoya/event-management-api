16fe94b234088283c3389cbd8c2ad00cbe142cc7
Felipe Ciro Montoya <felipeciromontoya.developer@gmail.com>
2024-09-14 21:15:34 -0500

    Updated index.js to reflect the new changes in routes, services, and event management logic.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const server = require("./src/server")
+const server = require("./src/server");
+const { conexion } = require("./db/db_conexion");
 
-server.start()
+conexion()
+  .then(() => {
+    console.log("Database connected");
+    server.start();
+  })
+  .catch((reason) => {
+    console.log("Error connecting to database", reason);
+    process.exit(1);
+  });
