d3d162ba119b4f45ab0ef036fd2436b5c2a75692
Felipe Ciro Montoya <felipeciromontoya.developer@gmail.com>
2024-09-15 09:36:46 -0500

    Fix: Updated event and user controllers with necessary modifications

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -1,58 +1,39 @@
 const { eventsService } = require("../services");
+const { catcherController } = require("../utils/catchers");
 
 module.exports = {
-  getAllEvents: async (req, res, next) => {
-    try {
-      const events = await eventsService.getAllEvents();
-      res.status(200).json(events);
-    } catch (error) {
-      next(error);
+  getAllEvents: catcherController(async (req, res) => {
+    const events = await eventsService.getAllEvents();
+    res.status(200).json(events);
+  }),
+  getEventById: catcherController(async (req, res) => {
+    const { id } = req.params;
+    const event = await eventsService.getEventById(id);
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
     }
-  },
-  getEventById: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const event = await eventsService.getEventById(id);
-      if (!event) {
-        return res.status(404).json({ message: "Event not found" });
-      }
-      res.status(200).json(event);
-    } catch (error) {
-      next(error);
+    res.status(200).json(event);
+  }),
+  createEvent: catcherController(async (req, res) => {
+    const eventData = req.body;
+    const newEvent = await eventsService.createEvent(eventData);
+    res.status(201).json(newEvent);
+  }),
+  updateEvent: catcherController(async (req, res) => {
+    const { id } = req.params;
+    const eventData = req.body;
+    const updatedEvent = await eventsService.updateEvent(id, eventData);
+    if (!updatedEvent) {
+      return res.status(404).json({ message: "Event not found" });
     }
-  },
-  createEvent: async (req, res, next) => {
-    try {
-      const eventData = req.body;
-      const newEvent = await eventsService.createEvent(eventData);
-      res.status(201).json(newEvent);
-    } catch (error) {
-      next(error);
+    res.status(200).json(updatedEvent);
+  }),
+  deleteEvent: catcherController(async (req, res) => {
+    const { id } = req.params;
+    const deletedEvent = await eventsService.deleteEvent(id);
+    if (!deletedEvent) {
+      return res.status(404).json({ message: "Event not found" });
     }
-  },
-  updateEvent: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const eventData = req.body;
-      const updatedEvent = await eventsService.updateEvent(id, eventData);
-      if (!updatedEvent) {
-        return res.status(404).json({ message: "Event not found" });
-      }
-      res.status(200).json(updatedEvent);
-    } catch (error) {
-      next(error);
-    }
-  },
-  deleteEvent: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const deletedEvent = await eventsService.deleteEvent(id);
-      if (!deletedEvent) {
-        return res.status(404).json({ message: "Event not found" });
-      }
-      res.status(200).json({ message: "Event deleted", data: deletedEvent });
-    } catch (error) {
-      next(error);
-    }
-  },
+    res.status(200).json({ message: "Event deleted", data: deletedEvent });
+  }),
 };
--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,58 +1,39 @@
 const { usersService } = require("../services");
+const { catcherController } = require("../utils/catchers");
 
 module.exports = {
-  getAllUsers: async (req, res, next) => {
-    try {
-      const users = await usersService.getAllUsers();
-      res.status(200).json(users);
-    } catch (error) {
-      next(error);
+  getAllUsers: catcherController(async (req, res) => {
+    const users = await usersService.getAllUsers();
+    res.status(200).json(users);
+  }),
+  getUserById: catcherController(async (req, res) => {
+    const { id } = req.params;
+    const user = await usersService.getUserById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  },
-  getUserById: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const user = await usersService.getUserById(id);
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      res.status(200).json(user);
-    } catch (error) {
-      next(error);
+    res.status(200).json(user);
+  }),
+  createUser: catcherController(async (req, res) => {
+    const userData = req.body;
+    const newUser = await usersService.createUser(userData);
+    res.status(201).json(newUser);
+  }),
+  updateUser: catcherController(async (req, res) => {
+    const { id } = req.params;
+    const userData = req.body;
+    const updatedUser = await usersService.updateUser(id, userData);
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
     }
-  },
-  createUser: async (req, res, next) => {
-    try {
-      const userData = req.body;
-      const newUser = await usersService.createUser(userData);
-      res.status(201).json(newUser);
-    } catch (error) {
-      next(error);
+    res.status(200).json(updatedUser);
+  }),
+  deleteUser: catcherController(async (req, res) => {
+    const { id } = req.params;
+    const deletedUser = await usersService.deleteUser(id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
     }
-  },
-  updateUser: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const userData = req.body;
-      const updatedUser = await usersService.updateUser(id, userData);
-      if (!updatedUser) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      res.status(200).json(updatedUser);
-    } catch (error) {
-      next(error);
-    }
-  },
-  deleteUser: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const deletedUser = await usersService.deleteUser(id);
-      if (!deletedUser) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      res.status(200).json({ message: "User deleted", data: deletedUser });
-    } catch (error) {
-      next(error);
-    }
-  },
+    res.status(200).json({ message: "User deleted", data: deletedUser });
+  }),
 };
