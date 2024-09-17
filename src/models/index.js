0e28f8edf4b54d8d0866716d09d1f6faa3f30d3d
Felipe Ciro Montoya <felipeciromontoya.developer@gmail.com>
2024-09-17 00:45:09 -0500

    Update model index to integrate new database logic

--- src/models/index.js
+++ src/models/index.js
@@ -9,7 +9,7 @@ module.exports = {
     status: Joi.string()
       .valid("scheduled", "cancelled", "completed")
       .required(),
-    image_url: Joi.string().max(255).optional(),
+    image_url: Joi.string().max(255).optional().allow(null, ""),
     location: Joi.object({
       name: Joi.string().required(),
       address: Joi.string().required(),
@@ -18,35 +18,49 @@ module.exports = {
       longitude: Joi.number(),
       image_url: Joi.string().max(255).optional(),
     }).required(),
-    created_by: Joi.object({
-      name: Joi.string().required(),
-      email: Joi.string().email().required(),
-      profile_image_url: Joi.string().uri(),
-      role: Joi.string().valid("organizer").default("organizer"),
-      status: Joi.string()
-        .valid("active", "inactive", "suspended")
-        .default("active"),
-    }).required(),
+    created_by: Joi.number().integer().optional(),
   }),
   userSchema: Joi.object({
     name: Joi.string().min(3).max(100).required(),
     email: Joi.string().email().required(),
-    profile_image_url: Joi.string().uri().optional(),
-    role: Joi.string().valid("admin", "user", "organizer").required(),
+    profile_image_url: Joi.string().uri().optional().allow(null, ""),
+    role: Joi.string().valid("admin", "user", "organizer").default("user"),
     status: Joi.string()
       .valid("active", "inactive", "suspended")
       .default("active"),
+    password: Joi.string()
+      .pattern(
+        new RegExp(
+          "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$"
+        )
+      )
+      .required(),
   }),
   ticketSchema: Joi.object({
     event_id: Joi.number().integer().required(),
     user_id: Joi.number().integer().optional(),
     ticket_type: Joi.string().valid("VIP", "General").required(),
     price: Joi.number().positive().required(),
-    purchase_date: Joi.date().iso(),
+    purchase_date: Joi.date()
+      .iso()
+      .default(() => new Date().toISOString()),
     status: Joi.string()
       .max(50)
       .valid("created", "reserved", "paid", "cancelled", "refunded")
       .default("created"),
+    enabled_days: Joi.array()
+      .items(
+        Joi.string().valid(
+          "sunday",
+          "monday",
+          "tuesday",
+          "wednesday",
+          "thursday",
+          "friday",
+          "saturday"
+        )
+      )
+      .required(),
   }),
   attenderSchema: Joi.object({
     event_id: Joi.number().integer().required(),
@@ -55,5 +69,11 @@ module.exports = {
     status: Joi.string()
       .valid("confirmed", "pending", "cancelled")
       .default("confirmed"),
+  }),
+  reservationSchema: Joi.object({
+    event_id: Joi.number().integer().required(),
+    ticket_type: Joi.string().valid("VIP", "General").required(),
+    price: Joi.number().positive().required(),
+    enabled_days: Joi.array().items(Joi.string()).required(),
   }),
 };
